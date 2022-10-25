5ca891b4463ee77d30cc0f1dbd326b0f2514249c
kairat-kempirbaev <7647704+kairat-kempirbaev@users.noreply.github.com>
2022-10-24 23:47:09 -0700

    Pet model with PostgresSQL

--- restify/models.py
+++ restify/models.py
@@ -11,3 +11,25 @@ class Specialties(models.Model):
 class VetSpecialties(models.Model):
     vet_id = models.ForeignKey(Vets, on_delete=models.CASCADE)
     specialty_id = models.ForeignKey(Specialties, on_delete=models.CASCADE)
+
+
+class Owners(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name =  models.CharField(max_length=30)
+    address = models.CharField(max_length=255)
+    city =  models.CharField(max_length=80)
+    telephone =  models.CharField(max_length=20)
+
+class Types(models.Model):
+    name = models.CharField(max_length=80)
+
+class Pets(models.Model):
+    name = models.CharField(max_length=30)
+    birth_date = models.DateField()
+    type_id = models.ForeignKey(Types, on_delete=models.CASCADE)
+    owner_id = models.ForeignKey(Owners, on_delete=models.CASCADE)
+
+class Visits(models.Model):
+    pet_id = models.ForeignKey(Pets, on_delete=models.CASCADE)
+    visit_date = models.DateField()
+    description = models.CharField(max_length=255)
