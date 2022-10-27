710d57a4493754bb325615aef00fb3c288bdfa28
kairat-kempirbaev <7647704+kairat-kempirbaev@users.noreply.github.com>
2022-10-26 21:27:11 -0700

    Use Django User model

--- pet_clinic/settings.py
+++ pet_clinic/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "restify"
+    "restify",
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# New user model specific for pet_clinic
+AUTH_USER_MODEL = 'restify.Users'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- restify/models.py
+++ restify/models.py
@@ -1,12 +1,18 @@
+from pyexpat import model
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import VetUserManager
 
-# Create your models here.
+
 class Vets(models.Model):
     first_name = models.CharField(max_length=30)
-    last_name =  models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
 
 class Specialties(models.Model):
     name = models.CharField(max_length=80)
+
 
 class VetSpecialties(models.Model):
     vet_id = models.ForeignKey(Vets, on_delete=models.CASCADE)
@@ -15,13 +21,15 @@ class VetSpecialties(models.Model):
 
 class Owners(models.Model):
     first_name = models.CharField(max_length=30)
-    last_name =  models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     address = models.CharField(max_length=255)
-    city =  models.CharField(max_length=80)
-    telephone =  models.CharField(max_length=20)
+    city = models.CharField(max_length=80)
+    telephone = models.CharField(max_length=20)
+
 
 class Types(models.Model):
     name = models.CharField(max_length=80)
+
 
 class Pets(models.Model):
     name = models.CharField(max_length=30)
@@ -29,7 +37,25 @@ class Pets(models.Model):
     type_id = models.ForeignKey(Types, on_delete=models.CASCADE)
     owner_id = models.ForeignKey(Owners, on_delete=models.CASCADE)
 
+
 class Visits(models.Model):
     pet_id = models.ForeignKey(Pets, on_delete=models.CASCADE)
     visit_date = models.DateField()
     description = models.CharField(max_length=255)
+
+
+class Users(AbstractUser):
+    # Alternative to is_active
+    username = models.CharField(max_length=20, unique=True)
+    enabled = models.BooleanField(default=True)
+    USERNAME_FIELD = 'username'
+
+    objects = VetUserManager()
+
+    def __str__(self):
+        return self.username
+
+
+class Roles(models.Model):
+    username = models.ForeignKey(Users, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20)
